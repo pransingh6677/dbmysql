9085e34cc39d47c91907f5bd239f87f3e0a249ce
Pran Singh <167063665+pransingh6677@users.noreply.github.com>
2025-03-08 18:10:14 +0530

    Create add_more_users.sql

---
+++ pran/add_more_users.sql
@@ -0,0 +1,6 @@
+-- Inserting more users into the 'users' table
+INSERT INTO users (name, email)
+VALUES
+    ('sonu', 'sonu.adams@example.com'),
+    ('sanu', 'sanu.brown@example.com'),
+    ('soni', 'soni.clark@example.com');
